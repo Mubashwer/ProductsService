91439a7a142bf4fff0e9064457fdbe61cc75cd5d
Mubashwer Khurshid <mubashwer.khurshid@xero.com>
2020-10-24 10:52:45 +1100

    Add SqlServer, product service and migrate db

---
+++ src/Products.API/Infrastructure/Migrations/20201023233405_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Products.API.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "products");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "MONEY", nullable: false),
+                    DeliveryPrice = table.Column<decimal>(type: "MONEY", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductOption",
+                schema: "products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    ProductId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductOption", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductOption_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalSchema: "products",
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductOption_ProductId",
+                schema: "products",
+                table: "ProductOption",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductOption",
+                schema: "products");
+
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "products");
+        }
+    }
+}
--- src/Products.API/Startup.cs
+++ src/Products.API/Startup.cs
@@ -1,8 +1,15 @@
+using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Products.API.Infrastructure.Services.ProductService;
+using Products.Domain.Aggregates.ProductAggregate;
+using Products.Infrastructure;
+using Products.Infrastructure.Repositories;
 
 namespace Products.API
 {
@@ -18,11 +25,15 @@ namespace Products.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddProductsDbContext(Configuration);
             services.AddControllers();
+
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IProductService, ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ProductsContext productsContext)
         {
             if (env.IsDevelopment())
             {
@@ -39,6 +50,29 @@ namespace Products.API
             {
                 endpoints.MapControllers();
             });
+
+            productsContext.Database.Migrate();
+        }
+    }
+
+    internal static class CustomExtensionsMethods
+    {
+        public static IServiceCollection AddProductsDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDbContext<ProductsContext>(options =>
+                    {
+                        options
+                            .UseLazyLoadingProxies()
+                            .UseSqlServer(configuration.GetConnectionString("Products"),
+                            sqlServerOptions =>
+                            {
+                                sqlServerOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                                sqlServerOptions.EnableRetryOnFailure(15, TimeSpan.FromSeconds(30), null);
+                            });
+                    }
+                );
+            return services;
         }
     }
 }

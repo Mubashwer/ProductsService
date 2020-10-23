1e11fc11b6b757b46ffa09f6378e831e607ff317
Mubashwer Khurshid <mubashwer.khurshid@xero.com>
2020-10-23 14:50:48 +1100

    Add skeleton Products.API project

---
+++ src/Products.API/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Products.API.Controllers
+{
+    [Route("products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}

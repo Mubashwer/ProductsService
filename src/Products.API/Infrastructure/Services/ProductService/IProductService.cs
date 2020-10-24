fb5e91d7e4565162aa09eefae1f418411777d49f
Mubashwer Khurshid <mubashwer.khurshid@xero.com>
2020-10-24 19:23:56 +1100

    refactor + complete product service

--- src/Products.API/Infrastructure/Services/ProductService/IProductService.cs
+++ src/Products.API/Infrastructure/Services/ProductService/IProductService.cs
@@ -7,13 +7,82 @@ namespace Products.API.Infrastructure.Services.ProductService
 {
     public interface IProductService
     {
+        /// <summary>
+        /// Gets paged list of products
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns>paged list of products</returns>
         Task<IPagedList<ProductDto>> GetPagedProductsAsync(int pageNumber, int pageSize);
+
+        /// <summary>
+        /// Gets paged list of product options of a product
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns>paged list of product options</returns>
         Task<IPagedList<ProductOptionDto>?> GetPagedProductOptionsAsync(Guid productId, int pageNumber,
             int pageSize);
 
+        /// <summary>
+        /// Finds product by id
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns>Added <see cref="ProductOptionDto"/> if found; null otherwise</returns>
         Task<ProductDto?> FindProductByIdAsync(Guid productId);
+
+        /// <summary>
+        /// Finds product option in product
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="productOptionId"></param>
+        /// <returns>Added <see cref="ProductOptionDto"/> if found; null otherwise</returns>
+        Task<ProductOptionDto?> FindProductOptionByIdAsync(Guid productId, Guid productOptionId);
+
+        /// <summary>
+        /// Adds product
+        /// </summary>
+        /// <param name="productDto"></param>
+        /// <returns>Added <see cref="ProductDto"/></returns>
         Task<ProductDto> AddProductAsync(ProductDto productDto);
-        Task UpdateProductAsync(ProductDto productDto);
-        Task DeleteProductAsync(ProductDto productDto);
+        
+        /// <summary>
+        /// Adds product option to a product
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="productOptionDto"></param>
+        /// <returns>Added <see cref="ProductOptionDto"/> if product is found; null otherwise</returns>
+        Task<ProductOptionDto?> AddProductOptionAsync(Guid productId, ProductOptionDto productOptionDto);
+
+        /// <summary>
+        /// Updates product
+        /// </summary>
+        /// <param name="productDto"></param>
+        /// <returns>true if updated; false if product is not found</returns>
+        Task<bool> UpdateProductAsync(ProductDto productDto);
+
+        /// <summary>
+        /// Updates product option
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="productOptionDto"></param>
+        /// <returns>true if updated; false if product or product option is not found</returns>
+        Task<bool> UpdateProductOptionAsync(Guid productId, ProductOptionDto productOptionDto);
+
+        /// <summary>
+        /// Deletes product by Id
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns>true if deleted; false if product is not found</returns>
+        Task<bool> DeleteProductAsync(Guid productId);
+
+        /// <summary>
+        /// Deletes product by Id
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="productOptionId"></param>
+        /// <returns>true if deleted; false if product or product option is not found</returns>
+        Task<bool> DeleteProductOptionAsync(Guid productId, Guid productOptionId);
     }
 }
--- src/Products.API/Infrastructure/Services/ProductService/ProductService.cs
+++ src/Products.API/Infrastructure/Services/ProductService/ProductService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Products.API.Application.Dtos;
 using Products.API.Extensions;
@@ -24,43 +26,32 @@ namespace Products.API.Infrastructure.Services.ProductService
             var products = await _productRepository.GetPagedAsync(pageNumber, pageSize);
             var productDtos = new PagedList<ProductDto>(products, products.Select(x => x.ToDto()));
 
-            _logger.LogDebug("Products fetched successfully");
             return productDtos;
         }
 
         public async Task<IPagedList<ProductOptionDto>?> GetPagedProductOptionsAsync(Guid productId, int pageNumber,
             int pageSize)
         {
-            var product = await _productRepository.FindByIdAsync(productId);
-
-            if (product is null)
-            {
-                _logger.LogInformation("Product with {ProductId} not found", productId);
-                return null;
-            }
+            var product = await FindProduct(productId);
+            if (product is null) return null;
 
             var productOptions = await product.ProductOptions.ToPagedListAsync(pageNumber, pageSize);
             var productOptionDtos =
-                new PagedList<ProductOptionDto>(productOptions, productOptions.Select(x => x.ToDto()));
+                new PagedList<ProductOptionDto>(productOptions, productOptions.Select(x => x.ToDto(productId)));
 
-            _logger.LogDebug("ProductOptions fetched successfully");
             return productOptionDtos;
         }
 
         public async Task<ProductDto?> FindProductByIdAsync(Guid productId)
         {
-            var product = await _productRepository.FindByIdAsync(productId);
-
-            if (product is null)
-            {
-                _logger.LogInformation("Product with {ProductId} not found", productId);
-            }
-            else
-            {
-                _logger.LogDebug("{Product} found", product);
-            }
-
+            var product = await FindProduct(productId);
             return product?.ToDto();
+        }
+
+        public async Task<ProductOptionDto?> FindProductOptionByIdAsync(Guid productId, Guid productOptionId)
+        {
+            var (_, productOption) = await FindProductAndProductOption(productId, productOptionId);
+            return productOption?.ToDto(productId);
         }
 
         public async Task<ProductDto> AddProductAsync(ProductDto productDto)
@@ -68,28 +59,117 @@ namespace Products.API.Infrastructure.Services.ProductService
             var product = await _productRepository.AddAsync(productDto.ToEntity());
             await _productRepository.UnitOfWork.SaveChangesAsync();
 
-            _logger.LogInformation("@{Product} added successfully", product);
+            _logger.LogInformation("@{ProductDto} added", productDto);
             return product.ToDto();
         }
 
-        public async Task UpdateProductAsync(ProductDto productDto)
+        public async Task<ProductOptionDto?> AddProductOptionAsync(Guid productId, ProductOptionDto productOptionDto)
         {
-            var product = productDto.ToEntity();
+            var product = await FindProduct(productId);
+            if (product is null) return null;
 
-            _productRepository.Update(product);
+            product.AddProductOption(productOptionDto.Id, productOptionDto.Name, productOptionDto.Description);
+
             await _productRepository.UnitOfWork.SaveChangesAsync();
 
-            _logger.LogInformation("@{Product} updated successfully", product);
+            _logger.LogInformation("@{ProductOptionDto} added", productOptionDto);
+            return product.ProductOptions.First(x => x.Id == productOptionDto.Id).ToDto(productId);
         }
 
-        public async Task DeleteProductAsync(ProductDto productDto)
+        public async Task<bool> UpdateProductAsync(ProductDto productDto)
         {
             var product = productDto.ToEntity();
+            _productRepository.Update(product);
+
+            try
+            {
+                await _productRepository.UnitOfWork.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (await _productRepository.FindByIdAsync(productDto.Id) is null)
+                {
+                    return false;
+                }
+
+                throw;
+            }
+
+            _logger.LogInformation("@{ProductDto} updated", productDto);
+            return true;
+        }
+
+        public async Task<bool> UpdateProductOptionAsync(Guid productId, ProductOptionDto productOptionDto)
+        {
+            var (product, productOption) = await FindProductAndProductOption(productId, productOptionDto.Id);
+
+            if (product is null || productOption is null) return false;
+
+            productOption.Update(productOptionDto.Name, productOptionDto.Description);
+            await _productRepository.UnitOfWork.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteProductAsync(Guid productId)
+        {
+            var product = await FindProduct(productId);
+            if (product is null) return false;
 
             _productRepository.Delete(product);
             await _productRepository.UnitOfWork.SaveChangesAsync();
 
-            _logger.LogInformation("@{Product} deleted successfully", product);
+            _logger.LogInformation("Product with {ProductId} is deleted", productId);
+            return true;
+        }
+
+        public async Task<bool> DeleteProductOptionAsync(Guid productId, Guid productOptionId)
+        {
+            var (product, productOption) = await FindProductAndProductOption(productId, productOptionId);
+
+            if (product is null || productOption is null) return false;
+
+            product.RemoveProductOption(productOption);
+            await _productRepository.UnitOfWork.SaveChangesAsync();
+
+            _logger.LogInformation("ProductOption with {ProductOptionId} in Product with {ProductId} is deleted",
+                productOptionId, productId);
+            return true;
+        }
+
+        private async Task<Product?> FindProduct(Guid productId)
+        {
+            var product = await _productRepository.FindByIdAsync(productId);
+
+            if (product is null)
+            {
+                _logger.LogInformation("Product with {ProductId} is not found", productId);
+                return null;
+            }
+
+            return product;
+        }
+
+        private async Task<Tuple<Product?, ProductOption?>> FindProductAndProductOption(Guid productId,
+            Guid productOptionId)
+        {
+            var product = await _productRepository.FindByIdAsync(productId);
+
+            if (product is null)
+            {
+                _logger.LogInformation("Product with {ProductId} is not found", productId);
+                return new Tuple<Product?, ProductOption?>(null, null);
+            }
+
+            var productOption = product.ProductOptions.FirstOrDefault(x => x.Id == productOptionId);
+
+            if (productOption is null)
+            {
+                _logger.LogInformation("ProductOption with {ProductOptionId} is not found in Product with {ProductId}",
+                    productOptionId, productId);
+                return new Tuple<Product?, ProductOption?>(product, null);
+            }
+
+            return new Tuple<Product?, ProductOption?>(product, productOption);
         }
     }
 }
